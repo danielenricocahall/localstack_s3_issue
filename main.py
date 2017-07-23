095973c4990f812e7fbdc1cfa426ac22040f5f7f
pavlo <pavlo@tubularlabs.com>
2017-07-23 16:28:48 +0300

    FIX update version

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import os
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.master("local").getOrCreate()
+S3 = 'http://aws-docker:4572'
+spark = SparkSession \
+    .builder \
+    .master('local') \
+    .config('spark.hadoop.fs.s3a.endpoint', S3) \
+    .getOrCreate()
 
 local_df = spark.read.json('/opt/local_s3/data/data.json')
 local_df.show()
 
-S3 = 'http://aws-docker:4572'
-os.system('aws --endpoint-url={} s3 mb s3://my-tests'.format(S3))
-os.system('aws --endpoint-url={} s3 sync data s3://my-tests/data'.format(S3))
+os.system('aws --endpoint-url={} s3 mb s3://my-test'.format(S3))
+os.system('aws --endpoint-url={} s3 sync data s3://my-test/data'.format(S3))
+os.system('aws --endpoint-url={} s3 ls s3://my-test/data/'.format(S3))
 
-remote_df = spark.read.json('s3://my-tests/data')
+remote_df = spark.read.json('s3://my-test/data')
 remote_df.show()
